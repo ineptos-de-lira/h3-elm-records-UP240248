784bd0b0e15744fc97b9955d90d93049dad7f418
UP240248 <up240248@alumnos.upa.edu.mx>
2026-02-15 22:25:32 -0600

    primer prueba

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,106 @@
 module Helper exposing (..)
+
+import Html
+
+
+add2 : Int -> Int -> Int
+add2 int1 int2 =
+    int1 + int2
+
+
+add3 : Float -> Float -> Float -> Float
+add3 int1 int2 int3 =
+    int1 + int2 + int3
+
+
+calc : Int -> Int -> (Int -> Int -> Int) -> Int
+calc int1 int2 operator =
+    operator int1 int2
+
+
+langs : List { name : String, releaseYear : Int, currentVersion : String }
+langs =
+    [ { name = "elm", releaseYear = 2012, currentVersion = "0.19.1" }
+    , { name = "javascript", releaseYear = 1995, currentVersion = "ECMAScript 2025" }
+    ]
+
+
+languageNames : List { name : String, releaseYear : Int, currentVersion : String } -> List String
+languageNames list =
+    List.map .name list
+
+
+users : List { name : String, uType : String }
+users =
+    [ { name = "Roberto", uType = "Student" }
+    , { name = "Mitsiu", uType = "Professor" }
+    ]
+
+
+onlyStudents : List { name : String, uType : String } -> List String
+onlyStudents list =
+    case list of
+        [] ->
+            []
+
+        x :: xs ->
+            if x.uType == "Student" then
+                x.name :: onlyStudents xs
+
+            else
+                "" :: onlyStudents xs
+
+
+type alias Videogame =
+    { title : String
+    , releaseYear : Int
+    , available : Bool
+    , downloads : Int
+    , genres : List String
+    }
+
+
+vg : List Videogame
+vg =
+    [ { title = "League of legends"
+      , releaseYear = 2009
+      , available = True
+      , downloads = 130000000
+      , genres = [ "rol", "estrategia", "jeugo en equipo" ]
+      }
+    , { title = "mortal kombat 1"
+      , releaseYear = 2024
+      , available = True
+      , downloads = 12000000
+      , genres = [ "Acción", "pelea" ]
+      }
+    ]
+
+
+getVideogameGenres : List Videogame -> List (List String)
+getVideogameGenres list =
+    List.map .genres list
+
+
+type alias Computer =
+    { ram : String, model : String, brand : String, screenSize : String }
+
+
+myLaptop : Computer
+myLaptop =
+    { ram = "1GB", model = "ThinkPad E14 Gen 7", brand = "Lenovo", screenSize = "15.6 inches" }
+
+
+main : Html.Html msg
+main =
+    Html.div []
+        [ Html.h1 [] [ Html.text "My laptop" ]
+        , Html.div []
+            [ Html.ul []
+                [ Html.li [] [ Html.text ("Ram: " ++ myLaptop.ram) ]
+                , Html.li [] [ Html.text ("Modelo: " ++ myLaptop.model) ]
+                , Html.li [] [ Html.text ("Marca: " ++ myLaptop.brand) ]
+                , Html.li [] [ Html.text ("Pulgadas: " ++ myLaptop.screenSize) ]
+                ]
+            ]
+        ]
